045f56596b08774948f127bfcd0504881a42e77e
MJ1546 <103990623+MJ1546@users.noreply.github.com>
2023-03-28 13:21:55 +0530

    server file

---
+++ server/db/db.js
@@ -0,0 +1,15 @@
+require('dotenv').config()  
+// const API_URL = "mongodb://127.0.0.1:27017/postdb";
+const API_URL = "mongodb+srv://manjunath-1546:mj123@cluster0.u0jyrui.mongodb.net/?retryWrites=true&w=majority";
+const { connect } = require('mongoose');
+
+async function connectWithDB () {
+    try {
+        await connect(process.env.API_URL || API_URL);
+        console.log("connected with DB")
+    } catch (err) {
+        console.log("not connect with db")
+    }
+}
+
+module.exports = connectWithDB;
